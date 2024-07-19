86436843aa4e37a08c624fff3d653bcfa8945080
Tek Raj Chhetri <tekrajchhetri@gmail.com>
2024-07-19 09:07:37 -0400

    docker file updated based on review comment + settings.py to use .env file

--- APItokenmanager/APIAuthManager/settings.py
+++ APItokenmanager/APIAuthManager/settings.py
@@ -18,7 +18,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 
 # Quick-start development settings - unsuitable for production
 # See https://docs.djangoproject.com/en/4.2/howto/deployment/checklist/
-env_file = os.path.join(BASE_DIR, ".env.development")
+env_file = os.path.join(BASE_DIR, ".env")
 
 # Load environment variables from the .env.development.development file
 load_dotenv(dotenv_path=env_file)
