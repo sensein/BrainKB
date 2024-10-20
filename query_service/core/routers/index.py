e567a62104dfa95b77b8f40427e4252f147ae18d
Tek Raj Chhetri <tekrajchhetri@gmail.com>
2024-10-20 17:08:52 -0400

    disabled endpoint from doc

--- query_service/core/routers/index.py
+++ query_service/core/routers/index.py
@@ -30,6 +30,6 @@ async def root():
     return {"message": "Welcome to FastAPI skeleton"}
 
 
-@router.get("/token-check", dependencies=[Depends(require_scopes(["read"]))])
+@router.get("/token-check", dependencies=[Depends(require_scopes(["read"]))], include_in_schema=False)
 async def token_check(user: Annotated[LoginUserIn, Depends(get_current_user)]):
     return {"message": "token check passed success"}
