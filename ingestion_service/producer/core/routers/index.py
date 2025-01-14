1585de8afe29d785eb93b1593e1174c3f6bbafda
Tek Raj Chhetri <52251022+tekrajchhetri@users.noreply.github.com>
2025-01-14 14:10:02 -0500

    new endpoint for health check added.

--- ingestion_service/producer/core/routers/index.py
+++ ingestion_service/producer/core/routers/index.py
@@ -19,6 +19,10 @@ from fastapi import APIRouter
 router = APIRouter()
 
 
-@router.get("/")
+@router.get("/home")
 async def root():
     return {"message": "Welcome to the producer ingest service"}
+
+@router.get("/health")
+async def health_check():
+    return {"status": "healthy"}
