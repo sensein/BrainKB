047b3f73b516bd2018101554bff1b7e96ef1b229
Tek Raj Chhetri <tekrajchhetri@gmail.com>
2025-02-18 17:36:21 -0500

    endpoint renamed and new endpoint to register the named graph added.

--- query_service/core/routers/insert.py
+++ query_service/core/routers/insert.py
@@ -21,18 +21,21 @@ from fastapi import APIRouter, Request, HTTPException, status
 from core.graph_database_connection_manager import insert_data_gdb
 import json
 import logging
-from core.pydantic_schema import InputKGTripleSchema
+from core.pydantic_schema import InputKGTripleSchema, NamedGraphSchema
 from typing import Annotated
 from core.models.user import LoginUserIn
 from core.security import get_current_user, require_scopes
 from fastapi import Depends
-from core.shared import convert_ttl_to_named_graph
-
+from core.shared import convert_ttl_to_named_graph, named_graph_metadata
+from core.graph_database_connection_manager import  fetch_data_gdb
+import datetime
 router = APIRouter()
 logger = logging.getLogger(__name__)
 
 
-@router.post("/insert/knowledge-graph-triples/", include_in_schema=True)
+@router.post("/knowledge-graph-triples/",
+             include_in_schema=True
+             )
 async def insert_knowledge_graph_triples(
     user: Annotated[LoginUserIn, Depends(get_current_user)],
         request: InputKGTripleSchema
@@ -54,5 +57,49 @@ async def insert_knowledge_graph_triples(
         raise HTTPException(
             status_code=status.HTTP_500_INTERNAL_SERVER_ERROR,
             detail="An error occurred processing the request",
+        )
+@router.post("/register-named-graph/")
+async def create_named_graph(
+        user: Annotated[LoginUserIn, Depends(get_current_user)],
+        request: NamedGraphSchema
+):
+    try:
+        data = json.loads(request.json())
+
+        # Ensure named_graph_url ends with '/'
+        named_graph_url = data['named_graph_url']
+        description = data['description']
+        if not named_graph_url.endswith('/'):
+            named_graph_url += '/'
+
+        query = f"""
+        ASK WHERE {{
+          GRAPH <https://brainkb.org/metadata/named-graph> {{
+            ?s ?p ?o.
+            FILTER(?s = <{named_graph_url}>)
+          }}
+        }}
+        """
+        named_graph_exists = fetch_data_gdb(query)
+        if not named_graph_exists["message"]["boolean"]:
+
+            response = insert_data_gdb(named_graph_metadata(
+                named_graph_url=named_graph_url,
+                description=description,
+                )
+            )
+            return response
+        else:
+            return "Graph is already registered."
+    except json.JSONDecodeError as e:
+        logger.error("JSON decoding failed", exc_info=True)
+        raise HTTPException(
+            status_code=status.HTTP_400_BAD_REQUEST, detail=f"Invalid JSON format {e}"
+        )
+    except Exception as e:
+        logger.error("An error occurred", exc_info=True)
+        raise HTTPException(
+            status_code=status.HTTP_500_INTERNAL_SERVER_ERROR,
+            detail=f"An error occurred processing the request {e}",
         )
 
